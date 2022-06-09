a4620f1d48200c789e15e937e087ce0b04e237bb
Varunkannaan <varunkannaan@gmail.com>
2022-06-09 15:46:20 +0530

    Pdf generator done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,73 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Pie, Line } from 'react-chartjs-2';
 
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, ArcElement, Tooltip, Legend);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+export const data1 = {
+    labels : ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'],
+    datasets: [
+        {
+            label: 'Expenses',
+            data: [2000,3000,5000,4000,8000,6000,1500,6400,9300,5600,2300,4700],
+            rderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+
+        },
+    ],
+};
+
+export const data2 = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
 
 export default function Dashboard() {
-    return(
+    return (
         <>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-5">
                 <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            </div>
+            <div className='w-96 h-96'>
+                <Pie data={data2} />;
+            </div>
+            <div className='w-1/2 h-46'>
+            <Line options={options} data = {data1}/>;
             </div>
         </>
     )
